566cf2f1a3843b5f4649bbba8d5eea739e3bd0a1
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-04-10 16:38:30 +0800

    Frontend prototype

---
+++ Code/UI/RomEntry.cs
@@ -0,0 +1,16 @@
+namespace sGBA;
+
+public sealed class RomEntry( string path, string displayTitle, string region,
+	string internalTitle, string gameCode, string publisher, string thumbnailUrl,
+	BaseFileSystem fileSystem )
+{
+	public string Path { get; } = path;
+	public string DisplayTitle { get; } = displayTitle;
+	public string Region { get; } = region;
+	public string InternalTitle { get; } = internalTitle;
+	public string GameCode { get; } = gameCode;
+	public string Publisher { get; } = publisher;
+	public string ThumbnailUrl { get; } = thumbnailUrl;
+	public BaseFileSystem FileSystem { get; } = fileSystem;
+}
+
